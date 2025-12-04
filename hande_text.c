2a36bbadd1ce2d3fe41c7228d7defc46aa485994
Easy-doro <vladimir.bleskine@gmail.com>
2025-12-04 09:56:47 +0100

    feat: Implement text and number handling for ft_printf
    - Add `hande_text.c` to handle string, char, and pointer conversions.
    - Implement `ft_hande_str`, `ft_handle_char`, and `ft_handle_ptr` functions.
    - Modify `handle_numbers.c` to include `ft_handle_nbrbase` for base conversions.

--- hande_text.c
+++ hande_text.c
@@ -1,36 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   handle_numbers.c                                   :+:      :+:    :+:   */
+/*   hande_text.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vlad <vlad@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/04 01:41:21 by vlad              #+#    #+#             */
-/*   Updated: 2025/12/04 09:55:33 by vlad             ###   ########.fr       */
+/*   Created: 2025/12/04 09:37:57 by vlad              #+#    #+#             */
+/*   Updated: 2025/12/04 09:53:48 by vlad             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_handle_int(int nb, t_struct *list)
+int	ft_hande_str(char *str, t_struct *list)
 {
-	int		len;
-	int 	ret_len;
+ 	int		len;
+	int		ret_len;
 	char	padding;
 
-	if (nb == 0 && list->precision == 0)
-		return (0);
-	len = ft_nbrlen(nb);
+	len = ft_strlen(str);
 	//precision
 	padding = ' ';
-	if (list->is_zero && list->precision < 0)
+	if (list->is_zero)
 		padding = '0';
 	ret_len = 0;
 	if (!list->is_minus)
-	{
+    {
 		ret_len += ft_put_padding(list->width, len, padding, list);
 	}
-	ret_len += ft_putnbr(nb, list);
+	ret_len += ft_putstr(str, list);
 	if (list->is_minus)
 	{
 		ret_len += ft_put_padding(list->width, len, padding, list);
@@ -38,20 +36,50 @@ int	ft_handle_int(int nb, t_struct *list)
 	return (ret_len);
 }
 
-int	ft_handle_nbrbase(unsigned int nb, char *base, t_struct *list)
+int	ft_handle_char(char c, t_struct *list)
 {
-	int	len;
-	int	ret_len;
-	char padding;
+	int		len;
+	int		ret_len;
+	char	padding;
+
+	len = 1;
+	//precision
+	padding = ' ';
+	if (list->is_zero)
+		padding = '0';
+	ret_len = 0;
+	if (!list->is_minus)
+    {
+		ret_len += ft_put_padding(list->width, len, padding, list);
+	}
+	ret_len += ft_putchar(c, list);
+	if (list->is_minus)
+    {
+		ret_len += ft_put_padding(list->width, len, padding, list);
+	}
+	return (ret_len);
+}
+
+int	ft_handle_ptr(void *ptr, t_struct *list)
+{
+ 	int		len;
+	int		ret_len;
+	char	padding;
 
 	len = ; //len ?
 	//precision
 	padding = ' ';
+	if (list->is_zero)
+		padding = '0';
 	ret_len = 0;
 	if (!list->is_minus)
+    {
 		ret_len += ft_put_padding(list->width, len, padding, list);
-	ret_len += ft_putnbrbase((unsigned long long)nb, base, list);
+	}
+	ret_len += ft_putptr(ptr, list);
 	if (list->is_minus)
+	{
 		ret_len += ft_put_padding(list->width, len, padding, list);
+	}
 	return (ret_len);
 }
